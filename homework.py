b6b2399feb05f9359b00f718479ee0a6e2690dbe
Роман Байкин <baykinroman@gmail.com>
2022-04-01 18:06:46 +0300

    updated bot

--- homework.py
+++ homework.py
@@ -8,25 +8,17 @@ import exceptions
 from logging import StreamHandler
 from dotenv import load_dotenv
 
-
-logging.basicConfig(format='%(asctime)s, %(levelname)s, %(message)s',
-                    level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-handler = StreamHandler(sys.stdout)
-logger.addHandler(handler)
 load_dotenv()
-
-
-PRACTICUM_TOKEN = os.getenv('praktikum_token')
-TELEGRAM_TOKEN = os.getenv('telegram_token')
-TELEGRAM_CHAT_ID = os.getenv('telegram_chat_id')
+PRACTICUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -38,8 +30,8 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except exceptions.MessageSendError as error:
-        logger.error(f'Сообщение не отправлено. Причина {error}')
-    logger.info('Сообщение отправлено')
+        logging.error(f'Сообщение не отправлено. Причина {error}')
+    logging.info('Сообщение отправлено')
 
 
 def get_api_answer(current_timestamp):
@@ -48,8 +40,8 @@ def get_api_answer(current_timestamp):
     params = {'from_date': timestamp}
     response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     if response.status_code != 200:
-        logger.error(f'Эндпоинт {ENDPOINT} недоступен.'
-                     f'Код сбоя {requests.get(ENDPOINT)}')
+        logging.error(f'Эндпоинт {ENDPOINT} недоступен.'
+                      f'Код сбоя {requests.get(ENDPOINT)}')
         raise exceptions.EndpointNotFound(f'Эндпоинт {ENDPOINT} недоступен.'
                                           f'Код сбоя {requests.get(ENDPOINT)}')
     return response.json()
@@ -57,41 +49,42 @@ def get_api_answer(current_timestamp):
 
 def check_response(response):
     """Проверка ответа от API."""
-    if not response['homeworks']:
-        logger.error('Ответ API не содержит homeworks')
+    if not isinstance(response, dict):
+        logging.error('Ответ API не содержит словарь')
+        raise TypeError('Ответ API не содержит словарь')
+    elif 'homeworks' not in response:
+        logging.error('Ответ API не содержит homeworks')
         raise KeyError('Ответ API не содержит homeworks')
-    homework = response.get('homeworks')
-    if not isinstance(homework, list):
+    elif not isinstance(response['homeworks'], list):
         raise TypeError('Получен иной элемент, чем список')
+    homework = response.get('homeworks')
     return homework
 
 
 def parse_status(homework):
     """Проверка статуса работы."""
     if homework.get('homework_name') is None:
-        logger.error('Ответ не содержит имя работы')
+        logging.error('Ответ не содержит имя работы')
         raise KeyError('Ответ не содержит имя работы')
     else:
         homework_name = homework.get('homework_name')
     if homework.get('status') is None:
-        logger.error('Ответ не содержит статус работы')
+        logging.error('Ответ не содержит статус работы')
         raise KeyError('Ответ не содержит статус работы')
     else:
         homework_status = homework.get('status')
-    if homework_status not in HOMEWORK_STATUSES:
-        logger.error('Словарь не содержит такого статуса')
+    if homework_status not in HOMEWORK_VERDICTS:
+        logging.error('Словарь не содержит такого статуса')
         raise KeyError('Словарь не содержит такого статуса')
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверка переменных доступа."""
-    ENV_VARS = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
-    for token in ENV_VARS:
-        if not token:
-            logger.critical('Один или несколько токенов отсутствуют')
-            return False
+    if not all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
+        logging.critical('Один или несколько токенов отсутствуют')
+        return False
     return True
 
 
@@ -100,33 +93,29 @@ def main():
     if not check_tokens():
         raise KeyError('Один или несколько токенов отсутствуют')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time()) - 60 * 60 * 24 * 30
-
-    compare_error = ''
-    compare_message = ''
+    current_timestamp = 1
 
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
-            message = parse_status(homework[0])
-            if compare_message != message:
-                compare_message = message
+            if homework and len(homework[0]):
+                message = parse_status(homework[0])
                 send_message(bot, message)
             else:
-                logger.debug('Новые статусы отсуствуют')
-            current_timestamp = int(time.time())
-            time.sleep(RETRY_TIME)
-
+                logging.info('Новые статусы отсуствуют')
+            current_timestamp = response.get('current_time')
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            if compare_error != message:
-                compare_error = message
-                send_message(bot, message)
-            else:
-                logger.debug('Ошибка повторяется')
+            send_message(bot, message)
+        finally:
             time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(handlers=[logging.StreamHandler()],
+                        level=logging.INFO,
+                        format='%(asctime)s, %(levelname)s, %(message)s')
+    handler = StreamHandler(sys.stdout)
+    logger = logging.getLogger(__name__)
     main()
